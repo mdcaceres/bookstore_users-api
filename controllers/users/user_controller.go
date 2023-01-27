a19662a8550e1f63094e0d3cb9594c3d422a1464
mdcaceres <you@example.com>
2023-01-26 22:51:10 -0300

    implement createUser

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,7 +1,11 @@
 package users
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/mdcaceres/bookstore_users-api/domain/users"
+	"github.com/mdcaceres/bookstore_users-api/service"
+	"github.com/mdcaceres/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
@@ -10,7 +14,22 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestErr("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, err := service.Create(user)
+	if err != nil {
+		fmt.Println("error in service create")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func SearchUser(c *gin.Context) {
