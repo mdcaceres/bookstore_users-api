1ab8e7af901b1b3caf33dcc72ba37f85e853ab07
mdcaceres <you@example.com>
2023-01-26 22:52:21 -0300

    add users service

--- service/users_service.go
+++ service/users_service.go
@@ -1,1 +1,10 @@
 package service
+
+import (
+	"github.com/mdcaceres/bookstore_users-api/domain/users"
+	"github.com/mdcaceres/bookstore_users-api/utils/errors"
+)
+
+func Create(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
