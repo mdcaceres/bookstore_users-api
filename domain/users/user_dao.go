ef0516e3879b4423155594a1b7ea0177a63c2240
mdcaceres <you@example.com>
2023-02-01 23:15:38 -0300

    add update method handle mysql errors

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,26 +1,20 @@
 package users
 
 import (
-	"fmt"
 	"github.com/mdcaceres/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/mdcaceres/bookstore_users-api/utils/dates"
 	"github.com/mdcaceres/bookstore_users-api/utils/errors"
-	"strings"
+	"github.com/mdcaceres/bookstore_users-api/utils/mysql_utils"
 )
 
 const (
-	UNIQUE_EMAIL         = "user.email"
-	NO_ROWS_IN_RESULTSET = "no rows in result set"
-	INSERT_USER          = "INSERT INTO user (first_name, last_name, email, date_created) VALUES (?,?,?,?);"
-	GET_USER             = "SELECT id, first_name, last_name, email, date_created FROM user WHERE id = ?;"
-)
-
-var (
-	usersDb = make(map[int64]*User)
+	InsertUser = "INSERT INTO user (first_name, last_name, email, date_created) VALUES (?,?,?,?);"
+	GetUser    = "SELECT id, first_name, last_name, email, date_created FROM user WHERE id = ?;"
+	UpdateUser = "UPDATE user SET first_name=?, last_name=?, email=? WHERE id=?;"
 )
 
 func (user *User) Get() *errors.RestErr {
-	stmt, err := users_db.Client.Prepare(GET_USER)
+	stmt, err := users_db.Client.Prepare(GetUser)
 	if err != nil {
 		return errors.NewInternalServerError(err.Error())
 	}
@@ -29,19 +23,16 @@ func (user *User) Get() *errors.RestErr {
 	result := stmt.QueryRow(user.Id)
 
 	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
-		if strings.Contains(err.Error(), NO_ROWS_IN_RESULTSET) {
-			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
-		}
-		return errors.NewInternalServerError(fmt.Sprintf("error while trying get user with id %d: %s ", user.Id, err.Error()))
+		return mysql_utils.ParseError(err)
 	}
 
 	return nil
 }
 
 func (user *User) Save() *errors.RestErr {
-	stmt, err := users_db.Client.Prepare(INSERT_USER)
+	stmt, err := users_db.Client.Prepare(InsertUser)
 	if err != nil {
-		return errors.NewInternalServerError("error when tying to save user")
+		return errors.NewInternalServerError(err.Error())
 	}
 	defer stmt.Close()
 
@@ -49,20 +40,30 @@ func (user *User) Save() *errors.RestErr {
 
 	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
 	if saveErr != nil {
-		if strings.Contains(saveErr.Error(), UNIQUE_EMAIL) {
-			return errors.NewBadRequestErr(
-				fmt.Sprintf("email %s is already exists", user.Email))
-		}
-		return errors.NewInternalServerError(
-			fmt.Sprintf("error when tying to save user : %s ", saveErr.Error()))
+		return mysql_utils.ParseError(saveErr)
 	}
 
 	userId, err := insertResult.LastInsertId()
 	if err != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("error when tying to save user : %s", err.Error()))
+		return mysql_utils.ParseError(saveErr)
 	}
 
 	user.Id = userId
+
+	return nil
+}
+
+func (user *User) Update() *errors.RestErr {
+	stmt, stmtErr := users_db.Client.Prepare(UpdateUser)
+	if stmtErr != nil {
+		return errors.NewInternalServerError(stmtErr.Error())
+	}
+	defer stmt.Close()
+
+	_, execErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.Id)
+	if execErr != nil {
+		return mysql_utils.ParseError(execErr)
+	}
 
 	return nil
 }
