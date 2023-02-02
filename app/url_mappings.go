22d18a97a0d8e16fefed6a562d11e5e985e8e7ad
mdcaceres <you@example.com>
2023-02-01 23:14:19 -0300

    add put url mapping add patch url mapping

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -13,4 +13,8 @@ func MapUrls() {
 	//router.GET("/users/search", controllers.SearchUser)
 
 	router.POST("/users", users.CreateUser)
+
+	router.PUT("/users/:user_id", users.UpdateUser)
+
+	router.PATCH("/users/:user_id", users.UpdateUser)
 }
